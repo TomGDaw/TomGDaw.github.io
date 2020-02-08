aa3bf34486f9051fe2ca6811b9393878fb16c667
TomGDaw <daw17004@byui.edu>
2020-02-07 19:21:09 -0700

    preston-5

---
+++ lesson5/js/date.js
@@ -0,0 +1,20 @@
+let d = new Date();
+
+document.getElementById("currentDate").textContent = d.getFullYear();
+
+//**************************/
+
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    };
+    document.getElementById("lastUpdated").textContent = new Date(document.lastModified).toLocaleDateString("en-US", options);
+} catch (e) {
+    alert('Error with code or your browser does not support Locale')
+}
